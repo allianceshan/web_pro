e9ca20cfb60456d6c2fe2c110cd9d696729efc9a
xuhang <xuhang@huitongjy.com>
2018-08-16 14:11:47 +0800

    【增加】加入注册文件以及点击注册跳转

--- HTTP/first_flask.py
+++ HTTP/first_flask.py
@@ -14,19 +14,28 @@ index = 0
 def hello_world():
     return render_template('index.html')
 
-@app.route('/loginsdf/')
-def login():
-    return '这是登录页面'
+@app.route('/regest/')
+def regest():
+    return render_template('regest.html')
 
 #设置请求参数 参数放在尖括号中
 @app.route('/artcle/<id>')
 def artcle(id):
     return '您请求的参数是' + id
 
-@app.route('/login_form',methods=['GET','POST'])
+@app.route('/login_form',methods=['POST'])
 def GetUserData():
     print("用户名： " + request.form['recipient-name'])
     print("密码：   " + request.form['message-text'])
     return '用户名：' + request.form['recipient-name'] + '  密码： ' + request.form['message-text'] 
+
+@app.route('/regest',methods=['POST'])
+def RegestUserData():
+    print("用户名：" + request.form['exampleInputUser'])
+    print('密码： ' + request.form['exampleInputPassword1'])
+    print("邮箱 "+ request.form['exampleInputEmail1'])
+    print("QQ号" + request.form["exampleInputQQ"])
+    return "注册成功"
+    
 if __name__=='__main__':
     app.run()
